c07c004e495ae5b2733fba4746de2ca1ec1dd457
Jose Ramon Mañes <joseramonmanesblasco@hotmail.com>
2020-05-24 14:10:08 +0200

    go 4 devops

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Let's Go!")
 	})
+
 	http.HandleFunc("/go", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Go 4 DevOps!")
 	})
